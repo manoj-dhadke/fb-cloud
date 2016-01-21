80b6a60c40c83a24f03f9adbfe3a5afc76549a8e
ketakikhude_infiverve <ketaki.khude@infiverve.com>
2016-01-21 19:17:44 +0530

    changes done as per new connector

--- digitalocean/operation/list_regions.rb
+++ digitalocean/operation/list_regions.rb
@@ -1,17 +1,17 @@
 #begin
 @log.trace("Started executing 'flint-cloud:digitalocean:operation:list_regions.rb' flintbit...")
 #Flintbit Input Parameters
-#Mandatory 
+#Mandatory
 @connector_name= @input.get("connector_name")		#Name of the DigitalOcean Connector
 @action = "list-regions"							#Action (list-regions)
 #optional
-@token = @input.get("token")						#token(credential of account) 
+@token = @input.get("token")						#token(credential of account)
 @request_timeout= @input.get("timeout")				#timeout
 
 @log.info("Flintbit input parameters are, connector name :: #{@connector_name} | action :: #{@action}| token :: #{@token}| timeout :: #{@request_timeout}")
 
 connector_call = @call.connector(@connector_name).set("action",@action).set("token",@token)
-                  
+
 if @request_timeout.nil? || @request_timeout.is_a?(String)
 	@log.trace("Calling #{@connector_name} with default timeout...")
 	response = connector_call.sync
@@ -27,7 +27,7 @@ response_message=response.message             #Execution status message
 #DigitalOcean Connector Response Parameters
 list = response.get("regions-list")            #list of machine
 
-if response.exitcode == 0  
+if response.exitcode == 0
 	@log.info("SUCCESS in executing #{@connector_name} where, exitcode :: #{response_exitcode} | message :: #{response_message}")
 	@log.info("#{@connector_name} list of machine :: #{list.to_s}")
 	@output.set("exit-code",0).set("message","success").setraw("regions-list",list.to_s)
