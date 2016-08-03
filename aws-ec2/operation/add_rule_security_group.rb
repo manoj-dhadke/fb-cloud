1c313ccb6fb79f9054d3e3457eb33bd76fef8c4a
Pratap Patil <pratap.patil@infiverve.com>
2016-08-03 19:52:28 +0530

    changed fields in add rule to security group flintbit

--- aws-ec2/operation/add_rule_security_group.rb
+++ aws-ec2/operation/add_rule_security_group.rb
@@ -2,24 +2,32 @@
 begin
 connector_name = @input.get("connector_name")
 direction = @input.get("direction")
-ip_range = @input.get("ip_range")
-cidr_block = @input.get("cidr_block")
 from_port = @input.get("from_port")
 to_port = @input.get("to_port")
 protocol = @input.get("protocol")
-group_id = @input.get("group_id")
+group_id = @input.get("security_group_id")
+
+# Optional
+ip_range = @input.get("ip_range")
+cidr_block = @input.get("cidr_block")
+region = @input.get("region")
+@access_key = @input.get('access-key')
+@secret_key = @input.get('security-key')
 
 @log.trace("Calling AWS-EC2 Connector...")
 response = @call.connector(connector_name)
-                .set("action","security-group-add-rule") #Contains the name of the operation
+                .set("action","security-group-add-rule")
+                .set("security-group-id",group_id)
                 .set("direction",direction)
                 .set("ip-range",ip_range)
                 .set("cidr-block",cidr_block)
                 .set("from-port",from_port)
-                .set("to-port",to_port)          #Amazon EC2 region,default region is "us-east-1"(Optional)
-                .set("protocol",protocol)        #Your security key (Optional)
-                .set("group-id",group_id)        #Your secret access key (Optional)
-                .timeout(100000)                 #Execution time of the Flintbit in milliseconds (Optional)
+                .set("to-port",to_port)
+                .set("protocol",protocol)
+                .set("region",region)
+                .set('access-key', @access_key)
+                .set('security-key', @secret_key)
+                .timeout(60000)
                 .sync
 
 #Amazon EC2 Connector Response Meta Parameters
