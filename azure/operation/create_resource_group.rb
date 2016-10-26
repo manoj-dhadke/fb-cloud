70fdd016f15b4c58d75ae8090d4d0e013eeece3e
Pratap Patil <pratap.patil@infiverve.com>
2016-10-26 18:16:29 +0530

    added azure operations flintbit

---
+++ azure/operation/create_resource_group.rb
@@ -0,0 +1,57 @@
+# begin
+@log.trace("Started executing 'fb-cloud:azure:operation:create_resource_group.rb' flintbit...")
+begin
+    # Flintbit Input Parameters
+    # Mandatory
+    @connector_name = @input.get('connector_name')
+    @region = @input.get('region')
+    @group_name = @input.get('group-name')
+    @action = 'create-resource-group'
+
+    # Optional
+    @request_timeout = @input.get('timeout')
+    @key = @input.get('key')
+    @tenant_id = @input.get('tenant-id')
+    @subscription_id = @input.get('subscription-id')
+    @client_id = @input.get('client-id')
+
+    @log.info("Flintbit input parameters are, action : #{@action} | Group name : #{@group_name} | Name : #{@name}")
+
+    connector_call = @call.connector(@connector_name)
+                          .set('action', @action)
+                          .set('tenant-id', @tenant_id)
+                          .set('subscription-id', @subscription_id)
+                          .set('key', @key)
+                          .set('client-id', @client_id)
+                          .set('group-name',@group_name)
+                          .set('region',@region)
+
+    if @connector_name.nil? || @connector_name.empty?
+        raise 'Please provide "MS Azure connector name (connector_name)" to create resource group'
+    end
+
+    if @request_timeout.nil? || @request_timeout.is_a?(String)
+        @log.trace("Calling #{@connector_name} with default timeout...")
+        response = connector_call.sync
+    else
+        @log.trace("Calling #{@connector_name} with given timeout #{@request_timeout}...")
+        response = connector_call.timeout(@request_timeout).sync
+    end
+
+    # Amazon EC2 Connector Response Meta Parameters
+    response_exitcode = response.exitcode	# Exit status code
+    response_message = response.message	# Execution status messages
+
+    if response_exitcode == 0
+        @log.info("SUCCESS in executing #{@connector_name} where, exitcode : #{response_exitcode} | message : #{response_message}")
+        @output.set('exit-code', 0).set('message', response_message).set('id',response.get('id'))
+    else
+        @log.error("ERROR in executing #{@connector_name} where, exitcode : #{response_exitcode} | message : #{response_message}")
+        @output.set('exit-code', 1).set('message', response_message)
+    end
+rescue Exception => e
+    @log.error(e.message)
+    @output.set('exit-code', 1).set('message', e.message)
+end
+@log.trace("Finished executing 'fb-cloud:azure:operation:create_resource_group.rb.rb' flintbit")
+# end
