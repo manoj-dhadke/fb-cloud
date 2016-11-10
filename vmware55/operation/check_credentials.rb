0023f492f598aa2cef1998d8156c429a03735b68
SomnathMule <somnathmulay@gmail.com>
2016-11-10 19:29:41 +0530

    vmware flintbits corrections as per check-list

--- vmware55/operation/check_credentials.rb
+++ vmware55/operation/check_credentials.rb
@@ -1,37 +1,39 @@
-@log.trace("Started execution 'flint-vmware:vc55:check_credential.rb' flintbit...") # execution Started
+# begin
+@log.trace("Started execution 'fb-cloud:vmware55:operation:check_credential.rb' flintbit...") # execution Started
 begin
-  # Flintbit input parametes
-    @connector_name = @input.get("connector_name") # "vmware"
-    @action = @input.get('action')
-    @username = @input.get('username')  #username of vcenter
-    @password=@input.get('password')    #password of vcenter
-    @url=@input.get('url')  
-
+    # Flintbit input parametes
+    # Mandatory
+    @connector_name = @input.get('connector_name') # vmware connector
+    @action = @input.get('action')     # action name
+    @username = @input.get('username') # username of vcenter
+    @password = @input.get('password') # password of vcenter
+    @url = @input.get('url')
 
     # calling vmware connector
     response = @call.connector(@connector_name)
-                    .set('action',@action)
-		    .set('url',@url)
-		    .set('username',@username)
-		    .set('password',@password)
+                    .set('action', @action)
+                    .set('url', @url)
+                    .set('username', @username)
+                    .set('password', @password)
                     .sync
 
-     response_exitcode = response.exitcode # Exit status code
-     response_message =  response.message # Execution status message
-    
-      if response_exitcode==0
-         @log.info("Success in executing #{@connector_name} Connector, where exitcode :: #{response_exitcode} | message :: #{response_message}")
-          @output.set('exit-code', 0).set('message', response_message.to_s)
-     
-      else
-         @log.error("ERROR in executing #{@connector_name} where, exitcode :: #{response_exitcode} | message :: #{response_message}")
-         @output.exit(1, response_message)
- 	 @output.set('exit-code', -1).set('message', response_message.to_s)
-      end
+    response_exitcode = response.exitcode # Exit status code
+    response_message =  response.message # Execution status message
+
+    if response_exitcode == 0
+        @log.info("Success in executing #{@connector_name} Connector, where exitcode :: #{response_exitcode} | message :: #{response_message}")
+        @output.set('exit-code', 0).set('message', response_message.to_s)
+
+    else
+        @log.error("ERROR in executing #{@connector_name} where, exitcode :: #{response_exitcode} | message :: #{response_message}")
+        @output.exit(1, response_message)
+        @output.set('exit-code', -1).set('message', response_message.to_s)
+    end
 
 rescue Exception => e
     @log.error(e.message)
     @output.set('exit-code', 1).set('message', e.message)
 end
 
-@log.trace("Finished execution 'flint-vmware:vc55:check_credential.rb' flintbit...") 
+@log.trace("Finished execution 'fb-cloud:vmware55:operation:check_credential.rb' flintbit...")
+# end
---
+++ vmware55/operation/list_virtual_machines.rb
@@ -0,0 +1,59 @@
+# begin
+@log.trace("Started execution 'fb-cloud:vmware55:operation:list_virtual_machines.rb' flintbit...") # execution Started
+begin
+
+    # Flintbit Input Parameters
+    # Mandatory
+    @connector_name = @input.get('connector_name') # vmware55 connector name
+    @action = @input.get('action') # name of action
+    @username = @input.get('username') # username of vmware55 connector
+    @password = @input.get('password') # password of vmware55 connector
+    @url = @input.get('url')
+
+    # Optional
+    request_timeout = @input.get('timeout')	# Execution time of the Flintbit in milliseconds (default timeout is 60000 milloseconds)
+
+    connector_call = @call.connector(@connector_name)
+                          .set('action', @action)
+                          .set('url', @url)
+                          .set('username', @username)
+                          .set('password', @password)
+
+    if @connector_name.nil? || @connector_name.empty?
+        raise 'Please provide "VMWare connector name (connector_name)" to list virtual machines'
+    end
+
+    if request_timeout.nil? || request_timeout.is_a?(String)
+        @log.trace("Calling #{@connector_name} with default timeout...")
+        # calling vmware55 connector
+        response = connector_call.sync
+    else
+        @log.trace("Calling #{@connector_name} with given timeout #{request_timeout}...")
+        # calling vmware55 connector
+        response = connector_call.timeout(request_timeout).sync
+    end
+
+    # VMWare  Connector Response Meta Parameters
+    response = response.to_s
+    response_exitcode = response.exitcode # Exit status code
+    response_message =  response.message # Execution status message
+
+    @log.info("RESPONSE :: #{response}")
+
+    if response_exitcode == 0
+        @log.info("Success in executing #{@connector_name} Connector, where exitcode :: #{response_exitcode} | message :: #{response_message}")
+        @output.set('result', response.to_s).set('exit-code', 0)
+
+    else
+        @log.error("ERROR in executing #{@connector_name} where, exitcode :: #{response_exitcode} | message :: #{response_message}")
+        @output.set('message', response_message.to_s).set('exit-code', -1)
+
+    end
+
+rescue Exception => e
+    @log.error(e.message)
+    @output.set('exit-code', -1).set('message', e.message)
+end
+
+@log.trace("Finished execution 'fb-cloud:vmware55:operation:list_virtual_machines.rb' flintbit...")
+# end
