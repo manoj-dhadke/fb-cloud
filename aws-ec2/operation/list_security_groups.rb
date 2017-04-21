5b5beb49e71ae44a0625adc5f0ccc62455c63c25
SomnathMule <somnathmulay@gmail.com>
2017-04-21 18:29:42 +0530

    change in list security group flintbit

--- aws-ec2/operation/list_security_groups.rb
+++ aws-ec2/operation/list_security_groups.rb
@@ -1,5 +1,5 @@
 # begin
-@log.trace("Started executing 'fb-cloud:aws-ec2:operation:list_security_group.rb' flintbit...")
+@log.trace("Started executing 'fb-cloud:aws-ec2:operation:list_security_groups.rb' flintbit...")
 
 # Flintbit Input Parameters
 # Mandatory
@@ -7,6 +7,7 @@ connector_name = @input.get('connector_name')	# Name of the Amazon EC2 Connector
 action = 'list-security-group' # Specifies the name of the operation:list-security-group
 
 # Optional
+region = @input.get('region') # Amazon EC2 region (default region is "us-east-1")
 @access_key = @input.get('access-key')
 @secret_key = @input.get('security-key')
 request_timeout = @input.get('timeout')	      # Execution time of the Flintbit in milliseconds (default timeout is 60000 milloseconds)
@@ -18,6 +19,9 @@ connector_call = @call.connector(connector_name)
                       .set('access-key', @access_key)
                       .set('security-key', @secret_key)
 
+if !region.nil? && !region.empty?
+    connector_call.set('region', region)
+end
 
 if request_timeout.nil? || request_timeout.is_a?(String)
     @log.trace("Calling #{connector_name} with default timeout...")
@@ -36,11 +40,11 @@ security_group_list = response.get('security-group-list')	# Set of Amazon EC2 se
 
 if response_exitcode == 0
     @log.info("SUCCESS in executing #{connector_name} where, exitcode : #{response_exitcode} | message : #{response_message}")
-    @output.setraw('security-group-list', security_group_list.to_s)
+    @output.set('exit-code', 0).set('message', response_message).setraw('security-group-list', security_group_list.to_s)
 else
     @log.error("ERROR in executing #{connector_name} where, exitcode : #{response_exitcode} |message : #{response_message}")
     @output.set('error', response_message)
     # @output.exit(1,response_message)						#Use to exit from flintbit
 end
-@log.trace("Finished executing 'fb-cloud:aws-ec2:operation:list_security_group.rb' flintbit")
+@log.trace("Finished executing 'fb-cloud:aws-ec2:operation:list_security_groups.rb' flintbit")
 # end
