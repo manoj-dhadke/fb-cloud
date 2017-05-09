fa8c0f26572cf53e9eb1bcc0927297ece9a33274
varun <varun.kulkarni@infiverve.com>
2017-05-09 15:21:20 +0530

    changes in delete load balancer flintbit

--- aws-ec2/operation/delete_application_lb_listener.rb
+++ aws-ec2/operation/delete_application_lb_listener.rb
@@ -16,17 +16,25 @@ begin
     @log.info("Flintbit input parameters are, connector_name:#{connector_name}  | action : #{action} | name: | listener ports: #{listeners}")
     connector_call = @call.connector(connector_name)
                           .set('action', action)
-                          .set('access_key',@access_key)
-                          .set('secret_key',@secret_key)
+                          .set('access-key',@access_key)
+                          .set('security-key',@secret_key)
                           .set('arn-list',listeners)
     #Cheking the region is not provided or not,if not then use default region as us-east-1
     if !region.nil? && !region.empty?
-        connector_call.set('region', region).sync
+        response = connector_call.set('region', region).sync
     else
         @log.trace("region is not provided so using default region 'us-east-1'")
+        response = connector_call.sync
     end
-    response_exitcode = connector_call.exitcode   # Exit status code
-    response_message = connector_call.message # Execution status messages
+    if request_timeout.nil? || request_timeout.is_a?(String)
+        @log.trace("Calling #{connector_name} with default timeout...")
+        response = connector_call.sync
+    else
+        @log.trace("Calling #{connector_name} with given timeout #{request_timeout}...")
+        response = connector_call.timeout(request_timeout).sync
+    end
+    response_exitcode = response.exitcode   # Exit status code
+    response_message = response.message # Execution status messages
         @log.info("exitcode : #{response_exitcode} | message : #{response_message}")
 # Cheking the response_exitcode,if it zero then show details and response_message otherwise show error_message to user
     if response_exitcode == 0
