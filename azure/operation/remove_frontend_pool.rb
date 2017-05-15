e0984fc9e4d16f873699d16a03929981972ca905
SomnathMule <somnathmulay@gmail.com>
2017-05-15 11:14:55 +0530

    added remove frontend pool flintbit

--- azure/operation/remove_frontend_pool.rb
+++ azure/operation/remove_frontend_pool.rb
@@ -1,5 +1,5 @@
 # begin
-@log.trace("Started executing 'fb-cloud:azure:operation:add_frontend_pool.rb' flintbit...")
+@log.trace("Started executing 'fb-cloud:azure:operation:remove_frontend_pool.rb' flintbit...")
 begin
     # Flintbit Input Parameters
     # Mandatory
@@ -7,8 +7,7 @@ begin
     @group_name = @input.get('group-name')
     @load_balancer_name = @input.get('load-balancer-name')
     @frontend_pool_name = @input.get('frontend-pool-name')
-    @public_ip_address_name = @input.get('public-ip-address-name')
-    @action = 'add-frontend-pool'
+    @action = 'remove-frontend-pool'
 
     # Optional
     @request_timeout = 180000
@@ -17,35 +16,27 @@ begin
     @subscription_id = @input.get('subscription-id')
     @client_id = @input.get('client-id')
 
-    @log.info("Flintbit input parameters are,connector-name:#{@connector_name} | action : #{@action}
-    | group-name :#{@group_name} | load-balancer-name : #{@load_balancer_name} | backend-pool-name:#{@backend_pool_name} ")
+    @log.info("Flintbit input parameters are, action : #{@action}")
 
 
     if @connector_name.nil? || @connector_name.empty?
-        raise 'Please provide "MS Azure connector name (connector_name)" to add frontend pool to load balancer'
+        raise 'Please provide "MS Azure connector name (connector_name)" to remove frontend pool from load balancer'
     end
 
 
     if @group_name.nil? || @group_name.empty?
-        raise 'Please provide "MS Azure group name (@group_name)" to add frontend pool to load balancer '
+        raise 'Please provide "MS Azure group name (@group_name)" to remove frontend pool from load balancer '
     end
 
 
     if @load_balancer_name.nil? || @load_balancer_name.empty?
-        raise 'Please provide "MS Azure load balancer name (@load_balancer_name)" to add frontend pool to load balancer'
+        raise 'Please provide "MS Azure load balancer name (@load_balancer_name)" to remove frontend pool from load balancer'
     end
 
 
     if @frontend_pool_name.nil? || @frontend_pool_name.empty?
-        raise 'Please provide "MS Azure frontend pool name (@frontend_pool_name)"to add frontend pool to load balancer'
+        raise 'Please provide "MS Azure frontend pool name (@frontend_pool_name)"to remove frontend pool from load balancer'
     end
-
-
-
-    if @public_ip_address_name.nil? || @public_ip_address_name.empty?
-        raise 'Please provide "MS Azure public ip address name (@public_ip_address_name)"to add frontend pool to load balancer'
-    end
-
 
       connector_call = @call.connector(@connector_name)
                           .set('action', @action)
@@ -54,7 +45,6 @@ begin
                           .set('group-name', @group_name)
                           .set('load-balancer-name', @load_balancer_name)
                           .set('frontend-pool-name', @frontend_pool_name)
-                          .set('public-ip-address-name', @public_ip_address_name)
 
     if @request_timeout.nil? || @request_timeout.is_a?(String)
         @log.trace("Calling #{@connector_name} with default timeout...")
@@ -81,5 +71,5 @@ rescue Exception => e
     @output.set('exit-code', 1).set('message', e.message)
 end
 
-@log.trace("Finished executing 'fb-cloud:azure:operation:add_frontend_pool.rb' flintbit")
+@log.trace("Finished executing 'fb-cloud:azure:operation:remove_frontend_pool.rb' flintbit")
 # end
