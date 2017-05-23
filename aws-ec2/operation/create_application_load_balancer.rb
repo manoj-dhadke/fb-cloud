453502d51c68eade87801fe121b15892c90218fe
varun <varun.kulkarni@infiverve.com>
2017-05-23 16:20:11 +0530

    FLT-777 resolved

--- aws-ec2/operation/create_application_load_balancer.rb
+++ aws-ec2/operation/create_application_load_balancer.rb
@@ -20,72 +20,80 @@ tags = @input.get('tags')
 
 @log.info("Flintbit input parameters are, action : #{action} |  Load Balancer Name : #{load_balancer_name} | Subnets : #{subnet_array} | Listeners : #{listener_array}")
 if !load_balancer_name.nil? && !load_balancer_name.empty?
-	if !@input.get('subnet1').nil? && !@input.get('subnet1').empty?
-		subnets = []
-		subnets << @input.get('subnet1')
-			if !@input.get('subnet2').nil? && !@input.get('subnet2').empty?
-				subnets << @input.get('subnet2')
-				connector_call = @call.connector(connector_name)
-		                          .set('action', action)
-		                          .set('name',load_balancer_name)
-		                          .set('access-key', @access_key)
-		                          .set('security-key', @secret_key)
-															.set('subnets',subnets)
+		name_validation = @call.connector(connector_name).set('action','describe-application-load-balancer').set('name',load_balancer_name)
+		validation_response = name_validation.sync
+		if validation_response.exitcode != 0
+
+								if !@input.get('subnet1').nil? && !@input.get('subnet1').empty?
+									subnets = []
+									subnets << @input.get('subnet1')
+										if !@input.get('subnet2').nil? && !@input.get('subnet2').empty?
+											subnets << @input.get('subnet2')
+											connector_call = @call.connector(connector_name)
+									                          .set('action', action)
+									                          .set('name',load_balancer_name)
+									                          .set('access-key', @access_key)
+									                          .set('security-key', @secret_key)
+																						.set('subnets',subnets)
+
+																if !request_timeout.nil? && !request_timeout.empty?
+																	connector_call.timeout(request_timeout)
+																else
+																	@log.trace("Calling #{connector_name} with default timeout...")
+																end
+																if !region.nil? && !region.empty?
+														         response = connector_call.set('region', region).sync
+														    else
+														    	response = connector_call.sync
+														        @log.trace("region is not provided so using default region 'us-east-1'")
+														    end
+										else
+											raise "Please provide at least two subnets"
+										end
+
+							elsif !subnet_array.nil? && !subnet_array.empty?
+								connector_call = @call.connector(connector_name)
+								                          .set('action', action)
+								                          .set('name',load_balancer_name)
+								                          .set('access-key', @access_key)
+								                          .set('security-key', @secret_key)
+									                      .set('listeners',listener_array)
+
+
+									if !subnet_array.nil? && !subnet_array.empty?
+									    connector_call.set('subnets',subnet_array)
+									else
+											raise "Subnets is not provided"
+									end
+									if !scheme.nil? && !scheme.empty?
+										connector_call.set('scheme',scheme)
+									end
 
 									if !request_timeout.nil? && !request_timeout.empty?
 										connector_call.timeout(request_timeout)
 									else
 										@log.trace("Calling #{connector_name} with default timeout...")
 									end
+
+									if !security_groups.nil? && security_groups.empty?
+										connector_call.set('security-groups',security_groups)
+									end
+
+									if !tags.nil? && tags.empty?
+										connector_call.set('tags',tags)
+									end
 									if !region.nil? && !region.empty?
-							         response = connector_call.set('region', region).sync
-							    else
-							    	response = connector_call.sync
-							        @log.trace("region is not provided so using default region 'us-east-1'")
-							    end
-			else
-				raise "Please provide at least two subnets"
-			end
-	elsif !subnet_array.nil? && !subnet_array.empty?
-		connector_call = @call.connector(connector_name)
-		                          .set('action', action)
-		                          .set('name',load_balancer_name)
-		                          .set('access-key', @access_key)
-		                          .set('security-key', @secret_key)
-			                      .set('listeners',listener_array)
-
-
-			if !subnet_array.nil? && !subnet_array.empty?
-			    connector_call.set('subnets',subnet_array)
-			else
-					raise "Subnets is not provided"
-			end
-			if !scheme.nil? && !scheme.empty?
-				connector_call.set('scheme',scheme)
-			end
-
-			if !request_timeout.nil? && !request_timeout.empty?
-				connector_call.timeout(request_timeout)
-			else
-				@log.trace("Calling #{connector_name} with default timeout...")
-			end
-
-			if !security_groups.nil? && security_groups.empty?
-				connector_call.set('security-groups',security_groups)
-			end
-
-			if !tags.nil? && tags.empty?
-				connector_call.set('tags',tags)
-			end
-			if !region.nil? && !region.empty?
-		         response = connector_call.set('region', region).sync
-		    else
-		    	response = connector_call.sync
-		        @log.trace("region is not provided so using default region 'us-east-1'")
-		    end
-	else
-		raise "Please provide subnets"
-	end
+								         response = connector_call.set('region', region).sync
+								    else
+								    	response = connector_call.sync
+								        @log.trace("region is not provided so using default region 'us-east-1'")
+								    end
+							else
+								raise "Please provide subnets"
+							end
+		else
+				raise "Load balancer with the given name '#{load_balancer_name}' already exists"
+		end
 else
 	@log.error("Error: At 'Load balancer name' #{load_balancer_name}. Please provide load balancer name.")
 end
