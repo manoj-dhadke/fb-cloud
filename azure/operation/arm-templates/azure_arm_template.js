502b18f133dafb5d2ab6c2b47321f17f01118b37
root <root@LAPTOP-LQRBCNTA.localdomain>
2018-10-26 13:32:42 +0530

    updated

--- azure/operation/arm-templates/azure_arm_template.js
+++ azure/operation/arm-templates/azure_arm_template.js
@@ -118,10 +118,12 @@ try {
             action = input.get('azure-arm-lbs-vm-config').get('action')
             log.trace(connector_name)
             resource_group_name = input.get('azure-arm-lbs-vm-config').get('resource_group_name')
+            deployment_name = input.get('azure-arm-lbs-vm-config').get('deployment_name')
+
 
             template = input.get('azure-arm-lbs-vm-config').get('template')
             template_parameters = util.json(input.get('azure-arm-lbs-vm-config').get('parameters'))
-            
+
             log.trace("Before connector call")
             connector_response = call.connector(connector_name)
                 .set('template', template)
