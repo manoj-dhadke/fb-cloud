049d5171d4e4c273fde94153e82a10e4dee7e943
root <root@LAPTOP-LQRBCNTA.localdomain>
2018-10-26 12:11:10 +0530

    updated flintbit

--- azure/operation/arm-templates/azure_arm_template.js
+++ azure/operation/arm-templates/azure_arm_template.js
@@ -14,30 +14,22 @@ try {
 
     // Service configuration Inputs
     stack_name = input.get('azure-arm-vm-config').get('stack-name')
-    connector_name = input.get('azure-arm-vm-config').get('connector-name')
-    action = input.get('azure-arm-vm-config').get('action')
-    log.trace(connector_name)
-    template = input.get('azure-arm-vm-config').get('template')
-   
-    template_parameters = util.json(input.get('azure-arm-vm-config').get('parameters'))
-
-    // Azure Credentials
-    client_id = input.get('azure-arm-vm-config').get('client-id')
-    azure_key = input.get('azure-arm-vm-config').get('key')
-    subscription_id = input.get('azure-arm-vm-config').get('subscription-id')
-    tenant_id = input.get('azure-arm-vm-config').get('tenant-id')
-    //
-
-
-    // Service form
-    deployment_name = input.get('deployment_name')
-    resource_group_name = input.get('resource_group_name')
-
 
     switch (stack_name) {
         case 'Ubuntu VM':
-            log.trace("util.json first :: " + template_parameters)
+            log.trace("Inside Ubuntu VM switch case")
+            // Service config
+            connector_name = input.get('azure-arm-vm-config').get('connector-name')
+            action = input.get('azure-arm-vm-config').get('action')
+            log.trace(connector_name)
+            template = input.get('azure-arm-vm-config').get('template')
+            template_parameters = util.json(input.get('azure-arm-vm-config').get('parameters'))
 
+            // Azure Credentials
+            client_id = input.get('azure-arm-vm-config').get('client-id')
+            azure_key = input.get('azure-arm-vm-config').get('key')
+            subscription_id = input.get('azure-arm-vm-config').get('subscription-id')
+            tenant_id = input.get('azure-arm-vm-config').get('tenant-id')
 
             // Service form parameter inputs?
             // location = input.get('location')
@@ -59,16 +51,14 @@ try {
             // diagnosticsStorageAccountType = input.get('diagnosticsStorageAccountType')
             // diagnosticsStorageAccountKind = input.get('diagnosticsStorageAccountKind')
 
-            // These variable names will be used when the following three parameters are taken from service config
-            // virtualMachineSize = input.get('virtualMachineSize')
-            // adminUsername = input.get('adminUsername')
-            // adminPassword = input.get('adminPassword')
-
+            // Service form
+            deployment_name = input.get('deployment_name')
+            resource_group_name = input.get('resource_group_name')
             virtualMachineSize = input.get('virtual_machine_size')
             adminUsername = input.get('admin_username')
             adminPassword = input.get('admin_password')
             netSecRules = template_parameters.get("networkSecurityGroupRules").get("value")
-            log.trace("type of netSecRules :: "+typeof netSecRules)
+            log.trace("type of netSecRules :: " + typeof netSecRules)
 
             template_parameters = JSON.parse(template_parameters)
             template_parameters["virtualMachineSize"]["value"] = virtualMachineSize
@@ -77,8 +67,8 @@ try {
             template_parameters["networkSecurityGroupRules"]["value"] = netSecRules
             template_parameters = util.json(template_parameters)
 
-            log.trace("Second util.json "+template_parameters)
-            
+            log.trace("Second util.json " + template_parameters)
+
             // Test code ends here
             log.trace("Before connector call")
             connector_response = call.connector(connector_name)
@@ -87,10 +77,10 @@ try {
                 .set('resource-group-name', resource_group_name)
                 .set('action', action)
                 .set('template-parameters', template_parameters)
-                .set('client-id',client_id)
-                .set('key',azure_key)
-                .set('subscription-id',subscription_id)
-                .set('tenant-id',tenant_id)
+                .set('client-id', client_id)
+                .set('key', azure_key)
+                .set('subscription-id', subscription_id)
+                .set('tenant-id', tenant_id)
                 .timeout(300000)
                 .sync()
 
@@ -99,18 +89,63 @@ try {
             exit_code = connector_response.get('exit-code')
             message = connector_response.get('message')
             if (exit_code == 0) {
-                user_message = "The request for a Ubuntu VM is completed. Here are the details. \n Deployment ID: "+connector_response.get('message')
+                user_message = "The request for a Ubuntu VM is completed. Here are the details. \n Deployment ID: " + connector_response.get('message')
                 log.trace("Exit code is " + exit_code)
                 log.trace("Response is :: " + connector_response)
-                output.set('user_message',user_message)
+                output.set('user_message', user_message)
             }
             else {
                 log.trace("Exit-Code :: " + exit_code + "\nMessage :: " + message)
                 output.set(connector_response)
             }
             break;
+
+        case "Complex":
+            log.trace("Inside moderate ARM template switch case")
+            // Azure credentials
+            client_id = input.get('azure-arm-lbs-vm-config').get('client-id')
+            azure_key = input.get('azure-arm-lbs-vm-config').get('key')
+            subscription_id = input.get('azure-arm-lbs-vm-config').get('subscription-id')
+            tenant_id = input.get('azure-arm-lbs-vm-config').get('tenant-id')
+
+            // Service config
+            connector_name = input.get('azure-arm-lbs-vm-config').get('connector-name')
+            action = input.get('azure-arm-lbs-vm-config').get('action')
+            log.trace(connector_name)
+            template = input.get('azure-arm-lbs-vm-config').get('template')
+            template_parameters = util.json(input.get('azure-arm-lbs-vm-config').get('parameters'))
+
+            log.trace("Before connector call")
+            connector_response = call.connector(connector_name)
+                .set('template', template)
+                .set('deployment-name', deployment_name)
+                .set('resource-group-name', resource_group_name)
+                .set('action', action)
+                .set('template-parameters', template_parameters)
+                .set('client-id', client_id)
+                .set('key', azure_key)
+                .set('subscription-id', subscription_id)
+                .set('tenant-id', tenant_id)
+                .timeout(300000)
+                .sync()
+
+            log.info("Connector call successfull")
+            
+            exit_code = connector_response.get('exit-code')
+            message = connector_response.get('message')
+            if (exit_code == 0) {
+                user_message = "The request for deployment of two virtual machines and a load balancer is completed. Here are the details for the request: \n Deployment ID: " + connector_response.get('message')
+                log.trace("Exit code is " + exit_code)
+                log.trace("Response is :: " + connector_response)
+                output.set('user_message', user_message)
+            }
+            else {
+                log.trace("Exit-Code :: " + exit_code + "\nMessage :: " + message)
+                output.set('user_message',connector_response)
+            }
+            break;
     }
 } catch (error) {
+    output.set('user_message',error)
     log.error(error)
-
 }
