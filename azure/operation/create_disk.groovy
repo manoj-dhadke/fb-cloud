f9b65bcc4937599f51cc03e8e1bf55d08f817c2a
anurag <anurag.dubey@infiverve.com>
2019-01-18 15:57:26 +0530

    generic flintbit for azure for disk create and delete operations

---
+++ azure/operation/create_disk.groovy
@@ -0,0 +1,66 @@
+log.trace("Started executing 'fb-cloud:azure:operation:create_disk.groovy' flintbit...")
+try{
+// Flintbit Input Parameters
+    // Mandatory  
+    connector_name = input.get('connector_name') //name of Azure connector
+    action = 'create-disk' //Specifies the name of the operation: create-disk
+    disk_name= input.get("disk-name") //name of the subnet which you want to create
+    size_in_gb = input.get("size-in-gb")
+    group_name = input.get("group-name")
+    region = input.get("region")
+    log.info("connector-name:${connector_name} | action :${action} | disk-name:${disk_name}")
+
+    //optional
+    key = input.get('key') //Azure accountid
+    tenant_id = input.get('tenant-id') //Azure account tenant-id
+    subscription_id = input.get('subscription-id') //Azure account subscription-id
+    client_id = input.get('client-id') //Azure client-id
+    request_timeout = input.get('timeout')	// Execution time of the Flintbit in milliseconds (default timeout is 60000 milloseconds)
+
+   //Checking that the connector name is provided or not,if not then throw exception with error message
+   if (connector_name == null || connector_name == ""){
+       throw new Exception ( 'Please provide "Azure connector name (connector_name)"')
+    }
+
+   //Checking that the dsik id is provided or not,if not then throw the exception with error message
+    if (disk_name == null || disk_name == ""){
+       throw new Exception ( 'Please provide "Disk Inamed"')
+    }
+
+        connector_call = call.connector(connector_name)
+                            .set('action', action)
+                            .set('tenant-id', tenant_id)
+                            .set('subscription-id', subscription_id)
+                            .set('key', key)
+                            .set('client-id', client_id)
+                            .set('data-disk-name',disk_name)
+                            .set('group-name', group_name)
+                            .set('size-in-gb', size_in_gb)
+                            .set('region', region)
+
+    if( request_timeout == null || request_timeout instanceof String ){
+        log.trace("Calling ${connector_name} with default timeout...")
+        response = connector_call.sync()
+     }
+    else{
+        log.trace("Calling ${connector_name} with given timeout ${request_timeout}...")
+        response = connector_call.timeout(request_timeout).sync()
+    }
+
+    // MS-azure Connector Response Meta Parameters
+    log.debug("==================>>>>>>>>>>>>>>>>>>>>>   ${response}")
+    response_exitcode = response.exitcode()	// Exit status code
+    response_message = response.message()	// Execution status messages
+
+    if (response_exitcode == 0){
+        log.info("SUCCESS in executing ${connector_name} where, exitcode : ${response_exitcode} | message : ${response_message}")
+        output.set('exit-code', 0).set('message', response_message)
+    } else {
+        log.error("ERROR in executing ${connector_name} where, exitcode : ${response_exitcode} | message : ${response_message}")
+        output.set('exit-code', 1).set('message', response_message)
+    }
+}catch (Exception  e){
+    log.error(e.message)
+    output.set('exit-code', -2).set('message', e.message)
+}
+log.trace("Finished executing 'fb-cloud:azure:operation:create_disk.groovy' flintbit")
