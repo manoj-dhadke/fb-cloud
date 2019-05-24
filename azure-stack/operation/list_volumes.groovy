b5224fdd26222db99fc5e6c7f23bb236aca7a54d
Varun_infiverve <varun.kulkarni@infiverve.com>
2019-05-24 21:28:56 +0530

    azure stack list disks

--- azure-stack/operation/list_volumes.groovy
+++ azure-stack/operation/list_volumes.groovy
@@ -1,5 +1,6 @@
-log.trace("Started executing 'fb-cloud:azure:operation:list_volumes.groovy' flintbit...")
-{
+log.trace("Started executing 'fb-cloud:azure-stack:operation:list_volumes.groovy' flintbit...")
+try{
+   
     //$ Flintbit Input Parameters
    //$ Mandatory
    connector_name = input.get('connector_name') //$name of Azure connector
@@ -12,6 +13,7 @@ log.trace("Started executing 'fb-cloud:azure:operation:list_volumes.groovy' flin
    subscription_id = input.get('subscription-id')// $Azure account subscription-id
    client_id = input.get('client-id')// $Azure client-id
    arm_endpoint = input.get('arm-endpoint')
+   request_timeout = 280000
    //$Checking that the connector name is provided or not,if not then raise the exception with error message
    if (connector_name!=null && connector_name == ""){
        raise 'Please provide "MS Azure Stack connector name (connector_name)" to list volume'
@@ -19,6 +21,7 @@ log.trace("Started executing 'fb-cloud:azure:operation:list_volumes.groovy' flin
     if (arm_endpoint!=null && arm_endpoint == ""){
         raise 'Please provide "MS Azure Stack ARM Endpoint(arm_endpoint)" to list volume'
    }
+
    connector_call = call.connector(connector_name)
                           .set('action', action)
                           .set('tenant-id', tenant_id)
@@ -33,31 +36,30 @@ log.trace("Started executing 'fb-cloud:azure:operation:list_volumes.groovy' flin
       connector_call.set('group-name',group_name)
     }
 
-    if (request_timeout!=null ){
+    if (input.get('timeout')!=null ){
         log.trace("Calling ${connector_name} with default timeout...")
         response = connector_call.sync()
-    }else
+    }else{
         log.trace("Calling ${connector_name} with given timeout ${request_timeout}...")
         response = connector_call.timeout(request_timeout).sync()
     }
-
+    log.info("Response of list disks ${response.toString()}")
     //$ MS-azure Connector Response Meta Parameters
-    response_exitcode = response.exitcode	//$ Exit status code
-    response_message = response.message	//$ Execution status messages
+    response_exitcode = response.exitcode()	//$ Exit status code
+    response_message = response.message()	//$ Execution status messages
 
     volume_list=response.get('volume-list')
 
-    if(response_exitcode == 0)
+    if(response_exitcode == 0){
         log.info("SUCCESS in executing ${connector_name} where, exitcode : ${response_exitcode} | message : ${response_message}")
         log.info("volume-details: ${response.toString()}")
         output.set('exit-code', 0).set('message', response_message).set('volume-list',volume_list)
-    }else
+    }else{
         log.error("ERROR in executing ${connector_name} where, exitcode : ${response_exitcode} | message : ${response_message}")
         output.set('exit-code', 1).set('message', response_message)
     }
-}catch(Exception e)
+}catch(Exception e){
     log.error(e.message)
     output.set('exit-code', 1).set('message', e.message)
 }
-log.trace("Finished executing 'fb-cloud:azure:operation:list_volumes.groovy' flintbit")
-
+log.trace("Finished executing 'fb-cloud:azure-stack:operation:list_volumes.groovy' flintbit")
